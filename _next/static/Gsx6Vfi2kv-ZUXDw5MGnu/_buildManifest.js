4ce111e9f8efdd86055385c5f297cb7452293fe3
Abhishek Anand <abhishekanand@192.168.1.4>
2025-01-14 17:05:47 +0530

    Updates

---
+++ _next/static/Gsx6Vfi2kv-ZUXDw5MGnu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/6b6ce1c1499cfd78.css","static/chunks/pages/index-f653f74f39a8396b.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
