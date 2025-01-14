d75b17a08c2b38bcaa5c733808ceeb1999be0a9d
Abhishek Anand <abhishekanand@192.168.1.4>
2025-01-14 17:13:49 +0530

    Updates

---
+++ _next/static/D2sJleI7YrG8EFTnr2bqc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/e960c2a57c59e08d.css","static/chunks/pages/index-8b12b05c168bc468.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/py7_Q9GLNf8XXXSrUvTpM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/1554dcf472997211.css","static/chunks/pages/index-56e10d718883677c.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
